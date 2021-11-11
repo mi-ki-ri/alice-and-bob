301aa75e6a12d1f898823310d5a5a2741021b5bf
HOTTA Tatsuya <hotta.tatsuya.fcc@gmail.com>
2021-11-11 16:22:27 +0900

    init

---
+++ app.py
@@ -0,0 +1,19 @@
+from graphviz import Digraph
+
+graph = Digraph(format="png" , comment="相関図")
+
+
+
+# ノードを追加
+graph.node("node1", label="ホッタ")
+graph.node("node2", label="ねこ")
+
+# 辺を追加
+graph.attr("edge", color="pink")
+graph.edge("node1", "node2", label="Like")
+graph.attr("edge", color="blue")
+graph.edge("node2", "node1", label="Dislike")
+graph.attr("edge", color="black")
+
+u = graph.unflatten(stagger=3)
+u.render("digraph")
